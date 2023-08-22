d8c0cb4941bdac530ecc8c0fbb7c4f46750ae333
Aadil Sengupta <53567039+PhantomZNeuroX@users.noreply.github.com>
2023-08-22 21:05:59 +0530

    Date Time Section

--- index.js
+++ index.js
@@ -185,3 +185,40 @@ new ScrollMagic.Scene({
 
   .addTo(controller); // assign the scene to the controller
 
+// Timer Scene//
+new ScrollMagic.Scene({
+  triggerElement: "#timer",
+  triggerHook: 0.8, // show, when scrolled 20% into view
+  duration: "100%", // hide 10% before exiting view (80% + 10% from bottom)
+  offset: 50 // move trigger to center of element
+})
+.setClassToggle(".date-sec", "visible") // add class to reveal
+.addIndicators() // add indicators (requires plug
+.addTo(controller);
+
+
+  /// Timer ///
+  function updateTimer() {
+    future  = Date.parse("September 28, 2023 9:30:00");
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("timer")
+      .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>minutes</span></div>' +
+        '<div>' + s + '<span>seconds</span></div>' ;
+  }
+  setInterval('updateTimer()', 1000 );
+  
